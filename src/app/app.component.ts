7ec0a7904e730e726964e5ec876dc51164e457a8
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-03-14 19:18:06 -0300

    Criando a Barra de Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'angular-rest-web';
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit(): void {
+
+    if (localStorage.getItem('token') == null) {
+      this.router.navigate(['login']);
+    }
+  }
+
+  public sair() {
+    localStorage.clear();
+    this.router.navigate(['login']);
+  }
 }
